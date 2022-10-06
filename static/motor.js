fe03a589f29b009e22c18047de74092b537cf186
luisdarb <luis-rubio@upc.edu.co>
2022-10-06 11:45:01 -0500

    Muchos cambios

--- static/motor.js
+++ static/motor.js
@@ -1,20 +1,87 @@
 
-const texto = document.getElementById("textohover")
+//javascript for navigation bar effect on scroll
 
-const asigntext = (event) => {
-    const textshow = {
-        1: "Ullamco qui excepteur laborum qui aute consequat nostrud sunt laborum qui irure sit consectetur. Amet ipsum eu id ex pariatur in enim do. Deserunt officia mollit tempor voluptate sint ipsum in nisi nulla ad minim. Fugiat nulla pariatur ullamco do cupidatat voluptate commodo pariatur ut reprehenderit et.",
-        2: "Laboris sit mollit cupidatat pa Eu esse labore aliquip eiusmod. Anim nostrud adipisicing sit ea cillum eiusmod do laboris ullamco sint.",
-        3: "In do consectetur ut eu elit sint elit id veniam enim ea. Reprehenderit irure nulla aute occaecat deserunt esse laboris qui. Nulla culpa veniam non aliquip amet deserunt enim aliqua cillum. Ut ut ex aute labore exercitation pariatur exercitation officia elit ex et proident elit. Cupidatat et cillum do mollit cupidatat in sint elit anim id."
-    }
-    const p = document.createElement("p")
-    p.style.display = 'inline'
-    p.textContent += textshow[event]
-    texto.appendChild(p)
+// window.addEventListener("scroll", function(){
+//   var header = document.querySelector("header");
+//   header.classList.toggle('down', window.scrollY > 0);
+
+
+//   //change logo
+//   var logo = document.querySelector(".brand img");
+//   if (window.scrollY>0) {
+//     logo.setAttribute('src', 'img/logo que chimba-07.png');
+//   }else{
+//     logo.setAttribute('src', 'img/Disco logo.png');
+//   }
+
+// });
+
+//javascript for responsive navigation sidebar menu
+var menu = document.querySelector('.menu');
+var menuBtn = document.querySelector('.menu-btn');
+var closeBtn = document.querySelector('.close-btn');
+
+menuBtn.addEventListener("click", () => {
+  menu.classList.add('active');
+});
+
+closeBtn.addEventListener("click", () => {
+  menu.classList.remove('active');
+});
+
+
+
+let slideIndex = 1;
+showSlides(slideIndex);
+
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
 }
 
-const removetext = () => {
-    texto.innerHTML = ""
+function currentSlide(n) {
+  showSlides(slideIndex = n);
 }
 
-removetext()
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
+
+
+
+
+// function showSlides2() {
+//   let i;
+//   let slides = document.getElementsByClassName("mySlides");
+//   for (i = 0; i < slides.length; i++) {
+//     slides[i].style.display = "none";
+//   }
+//   slideIndex++;
+//   if (slideIndex > slides.length) {slideIndex = 1}
+//   slides[slideIndex-1].style.display = "block";
+//   setTimeout(showSlides2, 4000); // Change image every 4 seconds
+// } 
+
+
+var canvasVideo = new CanvasVideoPlayer({
+  videoSelector: '.video',
+  canvasSelector: '.canvas',
+  timelineSelector: '.js-timeline',
+  framesPerSecond: 25,
+  autoplay: true,
+  audio: false,
+  makeLoop: true
+});
